3238642acdef23d21e3ace2d64227291bdca8ad6
YoussefMohamedSalah <edge.mohamed120@gmail.com>
2023-10-31 05:46:15 -0700

    test 2

--- src/App.tsx
+++ src/App.tsx
@@ -1,7 +1,10 @@
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import SignIn from "components/Auth/SignIn";
 import { AuthProvider } from "contexts/AuthContext";
 import { Suspense, lazy } from "react";
 import { Route, Routes } from "react-router-dom";
+import PasswordReset from "components/Auth/PasswordReset";
+import StepAuthentication from "components/Auth/StepAuthentication";
 
 const AuthIndex = lazy(() => import("./screens/AuthIndex"));
 const MainIndex = lazy(() => import("./screens/MainIndex"));
@@ -9,32 +12,20 @@ const MainIndex = lazy(() => import("./screens/MainIndex"));
 const queryClient = new QueryClient();
 
 const App: React.FC = () => {
-  return (
-    <QueryClientProvider client={queryClient}>
-      <AuthProvider>
-        <div id="mytask-layout" className="theme-indigo">
-          <Routes>
-            <Route
-              path="/dashboard/*"
-              element={
-                <Suspense fallback={null}>
-                  <MainIndex />
-                </Suspense>
-              }
-            />
-            <Route
-              path="/*"
-              element={
-                <Suspense fallback={null}>
-                  <AuthIndex />
-                </Suspense>
-              }
-            />
-          </Routes>
-        </div>
-      </AuthProvider>
-    </QueryClientProvider>
-  );
+  return <QueryClientProvider client={queryClient}>
+    <AuthProvider>
+      <div id="mytask-layout" className="theme-indigo">
+        <Routes>
+          <Route path="/dashboard/*" element={<Suspense fallback={null}>
+            <MainIndex />
+          </Suspense>} />
+          <Route path="/" element={<SignIn />} />
+          <Route path="/password-reset" element={<PasswordReset />} />
+          <Route path="/verify-email" element={<StepAuthentication />} />
+        </Routes>
+      </div>
+    </AuthProvider>
+  </QueryClientProvider>;
 };
 
 export default App;
